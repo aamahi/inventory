de6b1166920ee9b2ed66e7cb425e2a9cd157d9b1
mahi <amahi.wp@gmail.com>
2020-04-03 00:22:23 +0600

    Admin Dashboard Bladeing

---
+++ resources/views/admin/head.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mosaddek">
+    <meta name="keyword" content="FlatLab, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    {{--    <link rel="shortcut icon" href="{{asset('asset/img/favicon.html')}}">--}}
+
+    <title>FlatLab - Flat & Responsive Bootstrap Admin Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('asset/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('asset/css/bootstrap-reset.css')}}" rel="stylesheet">
+    <!--external css-->
+    <link href="{{asset('asset/assets/font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
+    <link href="{{asset('asset/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css')}}" rel="stylesheet" type="text/css" media="screen"/>
+    <link rel="stylesheet" href="{{asset('asset/css/owl.carousel.css')}}" type="text/css">
+{{--')}}--}}
+<!--right slidebar-->
+    <link href="{{asset('asset/css/slidebars.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+
+    <link href="{{asset('asset/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('asset/css/style-responsive.css')}}" rel="stylesheet" />
+
+</head>
